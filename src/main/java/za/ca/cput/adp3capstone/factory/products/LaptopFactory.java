96e70754c0e9250f344cfa5289a417064f7489ed
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-04-16 22:37:13 +0200

    add more parameters and checks

--- src/main/java/za/ca/cput/adp3capstone/factory/products/LaptopFactory.java
+++ src/main/java/za/ca/cput/adp3capstone/factory/products/LaptopFactory.java
@@ -1,10 +1,21 @@
 package za.ca.cput.adp3capstone.factory.products;
 
+import za.ca.cput.adp3capstone.domain.Category;
+import za.ca.cput.adp3capstone.domain.Seller;
 import za.ca.cput.adp3capstone.domain.products.Laptop;
 import za.ca.cput.adp3capstone.util.Helper;
 
 public class LaptopFactory {
-    public static Laptop createLaptop( String processor,
+    public static Laptop createLaptop( long productId,
+                                       Seller productSeller,
+                                       String productName,
+                                       Category category,
+                                       String description,
+                                       String imageUrl,
+                                       String brand,
+                                       double price,
+                                       boolean isSecondHand,
+                                       String processor,
                                        int ramSize,
                                        int storageSize,
                                        String storageType,
@@ -21,6 +32,14 @@ public class LaptopFactory {
                                        boolean hasWebcam,
                                        boolean isRefurbished)
     {
+        if (Helper.isStringNullOrEmpty(productName) ||
+                Helper.isStringNullOrEmpty(description) ||
+                Helper.isStringNullOrEmpty(imageUrl) ||
+                Helper.isStringNullOrEmpty(brand) ||
+                productSeller == null || category == null || price <= 0) {
+            return null;
+        }
+
         if (Helper.isStringNullOrEmpty(processor) ||
                 Helper.isStringNullOrEmpty(storageType) ||
                 Helper.isStringNullOrEmpty(graphicsCard) ||
@@ -29,6 +48,7 @@ public class LaptopFactory {
                 Helper.isStringNullOrEmpty(keyboardLayout)) {
             return null;
         }
+
         if (Helper.isIntNegative(ramSize) || Helper.isIntZero(ramSize)) return null;
         if (Helper.isIntNegative(storageSize) || Helper.isIntZero(storageSize)) return null;
         if (Helper.isDoubleNull(screenSize) || screenSize <= 0) return null;
@@ -37,6 +57,15 @@ public class LaptopFactory {
         if (Helper.isIntNegative(amountOfUsbPorts)) return null;
 
         return new Laptop.Builder()
+                .productId(productId)
+                .productSeller(productSeller)
+                .productName(productName)
+                .category(category)
+                .description(description)
+                .imageUrl(imageUrl)
+                .brand(brand)
+                .price(price)
+                .setIsSecondHand(isSecondHand)
                 .setProcessor(processor)
                 .setRamSize(ramSize)
                 .setStorageSize(storageSize)
