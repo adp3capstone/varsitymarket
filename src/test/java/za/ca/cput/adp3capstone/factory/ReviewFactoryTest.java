f2956ff05e82655e80f775d03c3f00f7596e461a
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-04-17 09:21:28 +0200

    Add product to be used in ReviewFactoryTest

--- src/test/java/za/ca/cput/adp3capstone/factory/ReviewFactoryTest.java
+++ src/test/java/za/ca/cput/adp3capstone/factory/ReviewFactoryTest.java
@@ -1,12 +1,13 @@
 package za.ca.cput.adp3capstone.factory;
 /* Product.java
-Product model class
+Review Factory class
 Author: Ethan Le Roux (222622172)
-Date:3 April 2025
+Date:17 April 2025
 */
 import org.junit.jupiter.api.Test;
 import za.ca.cput.adp3capstone.domain.Category;
 import za.ca.cput.adp3capstone.domain.Review;
+import za.ca.cput.adp3capstone.domain.Seller;
 import za.ca.cput.adp3capstone.domain.User;
 import za.ca.cput.adp3capstone.domain.products.Product;
 import za.ca.cput.adp3capstone.factory.products.LaptopFactory;
@@ -17,9 +18,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ReviewFactoryTest {
     User user = new User();
-    private Product product = null;
-    //private Product product = LaptopFactory.createLaptop(" ",);
-    private Review review = ReviewFactory.createReview(98901L,user,"This pc works!","I like it. good value for money", LocalDateTime.now(),5,product);
+    Product laptop = LaptopFactory.createLaptop(
+            1001L,
+            new Seller(),
+            "UltraTech ProBook 15",
+            Category.LAPTOP,
+            "A powerful laptop for developers and gamers.",
+            "https://example.com/images/laptop1.jpg",
+            "UltraTech",
+            15999.99,
+            false,
+            "Intel Core i7 11th Gen",
+            16,
+            512,
+            "SSD",
+            "NVIDIA GeForce RTX 3060",
+            15.6,
+            "1920x1080",
+            "Windows 11 Pro",
+            10.5,
+            1.8,
+            true,
+            "QWERTY",
+            3,
+            true,
+            true,
+            false
+            );
+    Review review = ReviewFactory.createReview(98901L,user,"This pc works!","I like it. good value for money", LocalDateTime.now(),5,laptop);
 
     @Test
     void createReview() {
