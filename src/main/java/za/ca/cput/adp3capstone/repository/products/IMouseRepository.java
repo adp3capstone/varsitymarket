fb298bff521f9a1c2c24c93042407f45d0ac54fb
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-04-17 10:21:28 +0200

    Fix imports

--- src/main/java/za/ca/cput/adp3capstone/repository/products/IMouseRepository.java
+++ src/main/java/za/ca/cput/adp3capstone/repository/products/IMouseRepository.java
@@ -1,16 +1,16 @@
 package za.ca.cput.adp3capstone.repository.products;
 
-import za.ca.cput.adp3capstone.domain.products.Mouse;
-import za.ca.cput.adp3capstone.domain.products.Product;
-import za.ca.cput.adp3capstone.repository.IRepository;
-
-import java.util.HashMap;
-
 /* IMouseRepository.java
 Mouse Repository interface
 Author: Ethan Le Roux (222622172)
 Date:17 April 2025
 */
+
+import za.ca.cput.adp3capstone.domain.products.Mouse;
+import za.ca.cput.adp3capstone.repository.IRepository;
+import java.util.HashMap;
+
+
 public interface IMouseRepository extends IRepository<Mouse, Long> {
     HashMap<Long, Mouse> getAll();
 }
