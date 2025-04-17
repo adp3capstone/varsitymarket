8a1f8efe9a7c64d57d2215b5480594de1e1adfb7
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-04-17 09:23:47 +0200

    Add product to be used in ReviewRepositoryTest

--- src/test/java/za/ca/cput/adp3capstone/repository/ReviewRepositoryTest.java
+++ src/test/java/za/ca/cput/adp3capstone/repository/ReviewRepositoryTest.java
@@ -7,9 +7,11 @@ Date:3 April 2025
 import org.junit.jupiter.api.*;
 import za.ca.cput.adp3capstone.domain.Category;
 import za.ca.cput.adp3capstone.domain.Review;
+import za.ca.cput.adp3capstone.domain.Seller;
 import za.ca.cput.adp3capstone.domain.User;
 import za.ca.cput.adp3capstone.domain.products.Product;
 import za.ca.cput.adp3capstone.factory.ReviewFactory;
+import za.ca.cput.adp3capstone.factory.products.LaptopFactory;
 
 import java.time.LocalDateTime;
 
@@ -18,10 +20,35 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ReviewRepositoryTest {
     private static IReviewRepository reviewRepository = ReviewRepository.getRepository();
-    User user = new User();
-    private Product product = null;
-    //private Product product = ProductFactory.createProduct(99999,"2 Year Old 2GB 50GB Thinkpad", Category.TECHNOLOGY,"Its an old celeron laptop.","https://varsitymarket.co.za/products/99999","Celeron",5999.99);
-    private Review review = ReviewFactory.createReview(98901L,user,"This pc works!","I like it. good value for money", LocalDateTime.now(),5,product);
+    private User user = new User();
+    private Product laptop = LaptopFactory.createLaptop(
+            1001L,
+            new Seller(),
+            "UltraTech ProBook 15",
+            Category.LAPTOP,
+            "A powerful laptop for developers and gamers.",
+            "https://example.com/images/laptop1.jpg",
+            "UltraTech",
+            15999.99,
+            false,
+            "Intel Core i7 11th Gen",
+            16,
+            512,
+            "SSD",
+            "NVIDIA GeForce RTX 3060",
+            15.6,
+            "1920x1080",
+            "Windows 11 Pro",
+            10.5,
+            1.8,
+            true,
+            "QWERTY",
+            3,
+            true,
+            true,
+            false
+    );
+    private Review review = ReviewFactory.createReview(98901L,user,"This pc works!","I like it. good value for money", LocalDateTime.now(),5,laptop);
 
     @Test
     @Order(1)
